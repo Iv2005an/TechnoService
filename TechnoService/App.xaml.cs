6e59ef4b651ed6ed5796d1a6758dfc67a7ab4893
Ivan Telitsin <telitsinivan05dev@yandex.ru>
2024-05-28 01:45:07 +0300

    Добавлено пустое приложение

---
+++ TechnoService/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace TechnoService;
+
+public partial class App : Application
+{
+    public App() => InitializeComponent();
+
+    private Window m_window;
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        m_window = new MainWindow();
+        m_window.Activate();
+    }
+}
