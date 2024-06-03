e432e9255a91727329d991112f8d9c73758d5986
Ivan Telitsin <telitsinivan05dev@yandex.ru>
2024-06-03 15:50:04 +0300

    Добавлен стандартный пользователь с правами администратора, со стандартным поролем.

--- TechnoService/Services/UsersService.cs
+++ TechnoService/Services/UsersService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
+using Microsoft.VisualBasic.Logging;
 using System;
 using System.Threading.Tasks;
 using TechnoService.Models;
@@ -22,6 +23,27 @@ public static class UsersService
             "password nvarchar(128) NOT NULL, " +
             "salt nvarchar(128) NOT NULL);", connection);
         await createUsersTableCommand.ExecuteNonQueryAsync();
+
+        using SqlCommand loginCountCommand = new($"SELECT COUNT(1) FROM users WHERE login=N'admin';", connection);
+        bool isAdminCreated = true;
+        using (SqlDataReader reader = await loginCountCommand.ExecuteReaderAsync())
+        {
+            if (await reader.ReadAsync()) isAdminCreated = reader.GetInt32(0) == 0;
+        }
+        if (isAdminCreated)
+        {
+            using SqlCommand addAdminCommand = new(
+            "INSERT INTO users VALUES(" +
+            "2," +
+            "N''," +
+            "N''," +
+            "N''," +
+            "N'admin'," +
+            $"N'D5CFC8FE8BC7C9612FC20ECBB8EC2C14993410CF47B7246D2D1EC176ED03CD4C07606F434403974CE1B44F050A09FF8FD2EC27FD63F84C4548A196661E18FDC2'," +
+            $"N'71DF5925D84C73C8BFCA22D41AB009A975627ACD4635A9EE44ADAC02AB9CA99E1CEC8E076D7C2934EF1E3FFC96ABCE71C039D217036331CDFBC29C6A4CE6BA57'" +
+            ");", connection);
+            await addAdminCommand.ExecuteNonQueryAsync();
+        }
     }
     public static async Task<bool> IsLoginFree(string login)
     {
